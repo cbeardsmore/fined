f3382f213428348994c18b0f7d67e3ba2a9415ba
Connor Beardsmore <connor.beardsmore@gmail.com>
2019-11-22 07:05:30 +0800

    Add Fines endpoint and basic helloworld function

--- handler.py
+++ handler.py
@@ -10,3 +10,14 @@ def fine(event, context):
         "statusCode": 200,
         "body": json.dumps(body)
     }
+
+def fines(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
