5a73ced86953c00f9a517ddc115dfa33f1200c6f
Connor Beardsmore <connor.beardsmore@gmail.com>
2019-11-23 18:54:19 +0800

    Add function to validate request via signing_key

---
+++ auth.py
@@ -0,0 +1,30 @@
+import os
+import time
+import hashlib
+import hmac
+
+HEADER_SLACK_TIMESTAMP = 'X-Slack-Request-Timestamp'
+HEADER_SLACK_SIGNATURE = 'X-Slack-Signature'
+BYTES_ENCODING = 'latin-1'
+
+def isVerifiedRequest(event):
+    slack_signing_secret = os.environ['SLACK_SIGNING_SECRET']
+    signature = event['headers'][HEADER_SLACK_SIGNATURE]
+    timestamp = event['headers'][HEADER_SLACK_TIMESTAMP]
+
+    if abs(time.time() - int(timestamp)) > 60 * 5:
+        print('Replay attack check failed. Time out by 5+ minutes')
+        return False
+
+    calculated_basestring = 'v0:{}:{}'.format(timestamp, event['body'])
+    calculated_signature = 'v0=' + hmac.new(
+        bytes(slack_signing_secret, BYTES_ENCODING),
+        msg = bytes(calculated_basestring, BYTES_ENCODING),
+        digestmod = hashlib.sha256
+    ).hexdigest()
+
+    if not hmac.compare_digest(signature, calculated_signature):
+        print('Invalid signature. Does not match X-Slack-Signature')
+        return False
+
+    return True
--- fine.py
+++ fine.py
@@ -1,14 +1,15 @@
-import json
-import urllib.parse
+from urllib.parse import parse_qs
+from auth import isVerifiedRequest
 
 def handle(event, context):
-    print(event['body'])
-    params = parse_qs(event['body'])
+    if not isVerifiedRequest(event):
+        return { "statusCode": 401 }
 
+    params = parse_qs(event['body'])
     text = params['text'][0]
     user_name = params['user_name'][0]
 
     print('Text -> ', text)
     print('User Name -> ', user_name)
-
     return { "statusCode": 200 }
+
