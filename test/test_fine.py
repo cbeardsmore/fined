47e92a806360c65e5b4a101859cc6681d1346029
Connor Beardsmore <connor.beardsmore@gmail.com>
2019-12-17 07:52:09 +0800

    Add simple pytest test with requirements.txt

--- test/test_fine.py
+++ test/test_fine.py
@@ -1,15 +1,10 @@
 import fine
-import time
 import json
-
 
 def test_is_verified_request_for_valid_request():
     event = []
     with open('local/fine.json') as file:
         event = json.load(file)
-
-    print(event)
-
     fine.handle(event, {})
-    assert 1 == 2
+    assert 1 == 1
 
