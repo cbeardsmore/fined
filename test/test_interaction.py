2e980bf6d7115812acfdbc6064762c42745448bc
Connor Beardsmore <connor.beardsmore@gmail.com>
2020-04-25 20:03:02 -0700

    Modal styling and tests (#32)
    * Style payment modal with pay/donate options
    
    * Use requests-mock to mock slack api calls and add simple modal test

--- test/test_interaction.py
+++ test/test_interaction.py
@@ -2,9 +2,9 @@ import json
 import os
 import pytest
 import interaction
-import response
 import utils
 import const
+import response
 
 @pytest.fixture(scope="function")
 def mock_os(monkeypatch):
@@ -27,13 +27,20 @@ def test_handle_with_unverified_request_returns_401(event):
     assert result['statusCode'] == 401
 
 
-def test_handle_sets_headers_and_status_code(event):
+def test_handle_returns_empty_response(event):
     result = interaction.handle(event, {})
     assert result['statusCode'] == 200
     assert result['headers']['Content-Type'] == 'application/json'
-
-
-def test_handle_returns_empty_response(event):
-    event = utils.update_signature(event)
-    result = interaction.handle(event, {})
     assert result['body'] == json.dumps('')
+
+
+def test_open_modal_calls_slack_view_open(requests_mock, event):
+    requests_mock.post(interaction.OPEN_VIEW_POST_URL)
+    interaction.open_modal(const.TRIGGER_ID)
+    last_request = requests_mock.last_request
+
+    assert requests_mock.call_count == 1
+    assert last_request.json() == response.create_pay_model(const.TRIGGER_ID)
+    assert last_request.headers['Content-Type'] == interaction.CONTENT_TYPE
+    assert last_request.headers['Authorization'] == 'Bearer {}'.format(const.BOT_ACCESS_TOKEN)
+ 
