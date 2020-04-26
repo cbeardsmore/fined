3d53421e6bd977d04aceaa6997f85caf682b0580
Connor Beardsmore <connor.beardsmore@gmail.com>
2020-04-26 12:45:27 -0700

    Only open model on specific action ID (#33)

--- interaction.py
+++ interaction.py
@@ -8,15 +8,19 @@ import response
 OPEN_VIEW_POST_URL = 'https://slack.com/api/views.open'
 CONTENT_TYPE = 'application/json; charset=utf-8'
 
+ACTION_PAY_FINE = 'pay_fine_button'
+
 def handle(event, _):
     if not auth.is_verified_request(event):
         return {'statusCode': 401}
 
-
     payload = json.loads(parse_qs(event['body'])['payload'][0])
-    print(payload)
-    open_modal_response = open_modal(payload['trigger_id'])
-    print(open_modal_response.content)
+    trigger_action = payload['actions'][0]
+    action_id = trigger_action['action_id']
+
+    if action_id == ACTION_PAY_FINE:
+        open_modal(payload['trigger_id'])
+
     return response.wrap_response_body('')
 
 
--- test/utils.py
+++ test/utils.py
@@ -1,5 +1,6 @@
 from urllib.parse import parse_qs, urlencode
 import time
+import json
 import auth
 import const
 
@@ -18,4 +19,20 @@ def set_body_text(body, text):
     params['text'] = text
     params['user_name'] = const.USERNAME
     params['team_id'] = const.TEAM_ID
+    return urlencode(params)
+
+
+def set_interaction_payload_field(body, field, value):
+    params = parse_qs(body)
+    payload = json.loads(params['payload'][0])
+    payload[field] = value
+    params['payload'] = json.dumps(payload)
+    return urlencode(params)
+
+
+def set_interaction_action_id(body, value):
+    params = parse_qs(body)
+    payload = json.loads(params['payload'][0])
+    payload['actions'][0]['action_id'] = value
+    params['payload'] = json.dumps(payload)
     return urlencode(params)
