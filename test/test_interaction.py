07150cb08f0eaf27e6abddb7a22f7d670a0a83a0
Connor Beardsmore <connor.beardsmore@gmail.com>
2020-04-26 19:38:11 -0700

    Empty resonse on view submission payloads and exclude node_modules

--- test/test_interaction.py
+++ test/test_interaction.py
@@ -14,43 +14,57 @@ def mock_os(monkeypatch):
 
 
 @pytest.fixture(scope="function")
-def event(mock_os):
+def event_pb(mock_os):
     event_payload = []
-    with open('local/interaction.json') as file:
+    with open('local/pay_button.json') as file:
         event_payload = json.load(file)
     return utils.update_signature(event_payload)
 
 
-def test_handle_with_unverified_request_returns_401(event):
-    event['headers'][const.HEADER_SLACK_SIGNATURE] = 'invalid'
-    result = interaction.handle(event, {})
+@pytest.fixture(scope="function")
+def event_vs(mock_os):
+    event_payload = []
+    with open('local/view_submission.json') as file:
+        event_payload = json.load(file)
+    return utils.update_signature(event_payload)
+
+
+def test_handle_with_unverified_request_returns_401(event_pb):
+    event_pb['headers'][const.HEADER_SLACK_SIGNATURE] = 'invalid'
+    result = interaction.handle(event_pb, {})
     assert result['statusCode'] == 401
 
 
-def test_handle_returns_empty_response(event):
-    result = interaction.handle(event, {})
+def test_handle_returns_empty_response(event_pb):
+    result = interaction.handle(event_pb, {})
     assert result['statusCode'] == 200
     assert result['headers']['Content-Type'] == 'application/json'
-    assert result['body'] == json.dumps('')
+    assert result['body'] == ''
 
 
-def test_handle_with_unknown_action_does_nothing(requests_mock, event):
-    event['body'] = utils.set_interaction_action_id(event['body'], 'random_action_id')
-    event = utils.update_signature(event)
+def test_handle_with_unknown_action_does_nothing(requests_mock, event_pb):
+    event_pb['body'] = utils.set_interaction_action_id(event_pb['body'], 'random_action_id')
+    event_pb = utils.update_signature(event_pb)
     requests_mock.post(interaction.OPEN_VIEW_POST_URL)
 
-    interaction.handle(event, {})
+    interaction.handle(event_pb, {})
     assert requests_mock.call_count == 0
 
-def test_handle_with_pay_action_calls_slack_view_open(requests_mock, event):
-    event['body'] = utils.set_interaction_action_id(event['body'], interaction.ACTION_PAY_FINE)
-    event = utils.update_signature(event)
+def test_handle_with_pay_action_calls_slack_view_open(requests_mock, event_pb):
+    event_pb['body'] = utils.set_interaction_action_id(event_pb['body'], interaction.ACTION_PAY_FINE)
+    event_pb = utils.update_signature(event_pb)
     requests_mock.post(interaction.OPEN_VIEW_POST_URL)
 
-    interaction.handle(event, {})
+    interaction.handle(event_pb, {})
     last_request = requests_mock.last_request
 
     assert requests_mock.call_count == 1
     assert last_request.json() == response.create_pay_model(const.TRIGGER_ID)
     assert last_request.headers['Content-Type'] == interaction.CONTENT_TYPE
     assert last_request.headers['Authorization'] == 'Bearer {}'.format(const.BOT_ACCESS_TOKEN)
+
+
+def test_handle_with_view_submission_does_nothing(requests_mock, event_vs):
+    requests_mock.post(interaction.OPEN_VIEW_POST_URL)
+    interaction.handle(event_vs, {})
+    assert requests_mock.call_count == 0
